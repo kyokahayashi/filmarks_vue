612781e1ce3df85891fcb037c8d48f797f70ec99
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2025-08-06 17:32:13 +0900

    不要なディレクトリを削除

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <ul>
-      <li>ルーターリンク</li>
-      <li>ルーターリンク</li>
-      <li>ルーターリンク</li>
+      <li><v-btn variant="tonal"> Filmarksの楽しみ方 </v-btn></li>
+      <li><v-btn variant="tonal"> 新規会員登録 </v-btn></li>
+      <li><v-btn variant="tonal"> ログイン </v-btn></li>
     </ul>
   </div>
   <div>
