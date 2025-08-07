7659f8430081e2bee74878e53c572798372bbd86
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2025-08-07 16:43:48 +0900

    動的映画詳細画面作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CreateReviewView from '@/views/CreateReviewView.vue'
+import DetailView from '@/views/DetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/reviewForm',
       name: 'reviewForm',
       component: CreateReviewView,
+    },
+    {
+      path: '/movie/:id',
+      name: 'MovieDetail',
+      component: DetailView
     }
     // {
     //   path: '/about',
