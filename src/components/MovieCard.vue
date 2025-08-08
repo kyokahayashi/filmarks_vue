788aea957960b660d82d5f5002bf561d04d052a0
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2025-08-08 15:57:13 +0900

    ベースのトップ画面作成した

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <v-card class="ma-4" elevation="2" max-width="344">
+    <v-img :src="movie.image" height="200px" cover></v-img>
+    <v-card-title>{{ movie.title }}</v-card-title>
+    <v-card-subtitle>{{ movie.year }}</v-card-subtitle>
+    <v-card-text>
+      {{ movie.description }}
+    </v-card-text>
+    <v-card-actions>
+      <v-btn> 詳細を見る </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  movie: {
+    type: Object,
+    required: true,
+  }
+})
+console.log('movieCardのprops', props)
+defineEmits(['view-detail'])
+</script>
 
 <style lang="scss" scoped></style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <v-container v-if="movies && movies.length">
+    <v-row>
+      <v-col
+        v-for="movie in movies"
+        :key="movie.id"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+      >
+        <MovieCard :movie="movie" @view-detail="handleDetail"></MovieCard>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+import MovieCard from './MovieCard.vue';
+const props = defineProps({
+  movies: {
+    type: Object,
+    required: true,
+  }
+})
 
-<style lang="scss" scoped></style>
+const emit = defineEmits(['select-movie'])
+const handleDetail = (movie) => {
+  emit('select-movie', movie);
+}
+</script>
