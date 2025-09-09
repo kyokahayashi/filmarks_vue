67597de7dda71d4e09d217ebac19c930467a9524
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2025-09-09 09:10:41 +0900

    認証状態によってログインボタンの表示切替を可能にした

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -1,28 +1,21 @@
 <template>
-  <v-layout class="overflow-visible" style="height: 56px;">
-    <v-bottom-navigation v-model="value" color="teal" grow>
-      <v-btn to="/search" value="search">
-        <v-icon>mdi-movie-search</v-icon>
-
-        映画を検索する
-      </v-btn>
-
-      <v-btn to="/popular" value="Popular">
-        <v-icon>mdi-movie</v-icon>
-
-        人気の映画
-      </v-btn>
-
-      <v-btn to="/reviewForm" value="reviewForm">
-        <v-icon>mdi-message-fast-outline</v-icon>
-
-        投稿したレビュー
-      </v-btn>
-    </v-bottom-navigation>
-  </v-layout>
+  <v-tabs v-model="tab" background-color="teal" dark grow fixed-tabs>
+    <v-tab to="/search">
+      <v-icon start>mdi-movie-search</v-icon>
+      映画を検索する
+    </v-tab>
+    <v-tab to="/popular">
+      <v-icon start>mdi-movie</v-icon>
+      人気の映画
+    </v-tab>
+    <v-tab to="/reviewForm">
+      <v-icon start>mdi-message-fast-outline</v-icon>
+      投稿したレビュー
+    </v-tab>
+  </v-tabs>
 </template>
 <script setup>
 import { ref } from 'vue'
 
-const value = ref(1)
+const tab = ref(null);
 </script>
